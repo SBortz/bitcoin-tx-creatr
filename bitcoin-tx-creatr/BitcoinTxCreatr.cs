7d0be8bb15ed0a3dba148a9345d99e9b8a38d7bd
SebastianBortz <s.bortz@neusta.de>
2018-01-11 13:23:11 +0100

    Switched to another command line library: CommandDotNet

---
+++ bitcoin-tx-creatr/BitcoinTxCreatr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandDotNet.Attributes;
+using NBitcoin;
+
+namespace bitcoin_tx_creatr
+{
+    public class BitcoinTxCreatr
+    {
+	    public void Create([Option(LongName = "json", ShortName = "j")]bool json = false)
+	    {
+			var tx = new Transaction();
+
+		    if (json)
+		    {
+				Console.WriteLine(tx.ToString());
+			}
+		    else
+		    {
+				Console.WriteLine(tx.ToHex());
+			}
+		}
+
+	    public int Show(string hexInput)
+	    {
+			if (string.IsNullOrWhiteSpace(hexInput))
+		    {
+			    Console.WriteLine("Please provide a transaction.");
+			    return 1;
+		    }
+
+		    Console.WriteLine("Here is your transaction:");
+
+		    var tx = new Transaction(hexInput);
+		    Console.WriteLine(tx);
+
+		    return 0;
+		}
+    }
+}
